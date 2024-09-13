b07c16c3dfbc7ec63604ceaa611e1cec42290fae
saifdin0101 <saifdin0808@gmail.com>
2024-09-13 16:20:28 +0100

    add json of the shop

--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+
 export const Shop = () => {
     return (
         <div className='pt-[100px]'>
@@ -7,11 +8,70 @@ export const Shop = () => {
                 <div>Products</div>
             </header>
             <div className='h-[200vh] w-[100%] flex justify-center items-center gap-10'>
-                <div className='h-[150vh] w-[250px] bg-blue-300'></div>
+                <div className='h-[150vh] w-[250px]  flex flex-col gap-20 p-4'>
+                    <div className='flex flex-col justify-center items-center'>
+                        <div className='text-2xl font-medium mb-4'>Categories</div>
+                        <div>
+                            <label className='container '>
+                                <input type='radio' name='category' value='sell' />
+                                <span className='checkmark'></span>
+                                <span className='ml-2 text-lg '>Sell</span>
+                            </label>
+                            <label className='container block mt-2'>
+                                <input type='radio' name='category' value='sold' />
+                                <span className='checkmark'></span>
+                                <span className='ml-2 text-lg'>Sold</span>
+                            </label>
+                            <label className='container block mt-2'>
+                                <input type='radio' name='category' value='new' />
+                                <span className='checkmark'></span>
+                                <span className='ml-2 text-lg'>New</span>
+                            </label>
+                        </div>
+                    </div>
+                    <div className='flex flex-col justify-center items-center'>
+                        <div className='text-2xl font-medium mb-4'>Color</div>
+                        <div>
+                            <label className='container'>
+                                <input type='radio' name='color' value='black' />
+                                <span className='checkmark'></span>
+                                <span className='ml-2 text-lg'>Black</span>
+                            </label>
+                            <label className='container block mt-2'>
+                                <input type='radio' name='color' value='white' />
+                                <span className='checkmark'></span>
+                                <span className='ml-2 text-lg'>White</span>
+                            </label>
+                            <label className='container block mt-2'>
+                                <input type='radio' name='color' value='blue' />
+                                <span className='checkmark'></span>
+                                <span className='ml-2 text-lg'>Blue</span>
+                            </label>
+                        </div>
+                    </div>
+                    <div className='flex flex-col justify-center items-center'>
+                        <div className='text-2xl font-medium mb-4'>Price</div>
+                        <div>
+                            <label className='container'>
+                                <input type='radio' name='price' value='0-5' />
+                                <span className='checkmark'></span>
+                                <span className='ml-2 text-lg'>$0 - $5</span>
+                            </label>
+                            <label className='container block mt-2'>
+                                <input type='radio' name='price' value='5-10' />
+                                <span className='checkmark'></span>
+                                <span className='ml-2 text-lg'>$5 - $10</span>
+                            </label>
+                            <label className='container block mt-2'>
+                                <input type='radio' name='price' value='10-20' />
+                                <span className='checkmark'></span>
+                                <span className='ml-2 text-lg'>$10 - $20</span>
+                            </label>
+                        </div>
+                    </div>
+                </div>
                 <div className='h-[150vh] w-[70vw] bg-blue-900'></div>
             </div>
-            
         </div>
     );
 };
-
